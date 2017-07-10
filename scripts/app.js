bc56707074ff4d5a9241579ea9802cf5a4126fd2
giri68 <giri.pdt@gmail.com>
2017-07-09 22:12:24 -0700

    geoquakes assignment done

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,16 @@
 // define globals
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
-
+var map;
 $(document).ready(function() {
   console.log("Let's get coding!");
   // CODE IN HERE!
+createmap();
+
   $.ajax({
     method: "GET",
-    url: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson",
-    dataType: 'json',
+    url: weekly_quakes_endpoint,
+    dataType: "json",
     success: onSuccess
-
   });
 
   //
@@ -18,11 +19,32 @@ $(document).ready(function() {
   // })
 });
 
-
-
 function onSuccess(responseData){
-  console.log("TEST", responseData);
-  var random = responseData.features[0].properties.title;
-  console.log("TEST 2", responseData.features[0].properties.title);
-  $('#info').append(random);
+  
+
+  let quakeFeatures = responseData.features;
+
+  console.log(responseData);
+  console.log(quakeFeatures[0].properties.title);
+  quakeFeatures.forEach(function(quakeFeaturesData){
+    var featuresTitle = quakeFeaturesData.properties.title;
+    var quakeHoursAgo = Math.round((Date.now() - quakeFeaturesData.properties.time) / (1000*60*60));
+$("#info").append(`<p> ${featuresTitle} (${quakeHoursAgo} hours ago) </p> <hr>`);
+var longitude = quakeFeaturesData.geometry.coordinates[0];
+var lattitude = quakeFeaturesData.geometry.coordinates[1];
+new google.maps.Marker({
+  position: new google.maps.LatLng(lattitude, longitude),
+  map: map,
+  title: featuresTitle
+});
+
+  });
+}
+
+
+function createmap(){
+  map = new google.maps.Map(document.getElementById("map"),{
+    center: {lat: 37.7749, lng: -122.4194},
+    zoom: 1
+  });
 }
